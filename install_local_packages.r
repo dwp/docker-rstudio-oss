c7b89763a1c87515bee8e1cfbe9013200febc607
Dragos Costache <dragos.costache@infinityworks.com>
2021-03-04 16:23:44 +0000

    Ammend local package installation script to check for newer version of packages

--- install_local_packages.r
+++ install_local_packages.r
@@ -4,22 +4,41 @@ USER_LIB_PATH=Sys.getenv("USER_LIB_PATH", unset=paste0("/home/", Sys.getenv("USE
 packages <- list.files(path=PACKAGES_PATH)
 library(devtools)
 library(stringr)
-
-is_installed <- function(package){
-    require(str_replace(package, "(.*?)\\..*$", replacement="\\1"), character.only = TRUE)
-}
+library(desc)
 
 dir.create(USER_LIB_PATH, showWarnings = FALSE)
 
+should_install <- function(package){
+    installed_packages <- installed.packages()
+    package_name <- str_replace(package, "(.*?)\\..*$", replacement="\\1")
+
+    if(!(package_name %in% installed_packages)){
+        print(paste0("Package ", package_name, " not currently installed. Installing..."))
+        return(TRUE)
+    }
+
+    tmp_extract_path = paste0("/tmp/pckg-install/")
+    dir.create(tmp_extract_path, showWarnings = FALSE)
+    untar(package, files = paste0(package_name, "/DESCRIPTION"), exdir=tmp_extract_path)
+
+    desc <- description$new(file=paste0(tmp_extract_path, package_name))
+    new_package_version <- toString(desc$get_version())
+    installed_package_version <- toString(installed_packages[package_name,  "Version"])
+
+    print(paste0(package_name, " - Installed package version: ", installed_package_version, ". New package version: ", new_package_version))
+
+    if(compareVersion(installed_package_version, new_package_version) == -1){
+        print(paste0("Package ", package_name, " is newer. Installing..."))
+        return(TRUE)
+    } else {
+        print(paste0("Package ", package_name, " is same version or older. Not installing."))
+        return(FALSE)
+    }
+}
+
 for(pck in packages){
-    if(!is_installed(pck)) {
+    if(should_install(pck)) {
         install_local(paste0(PACKAGES_PATH, "/", pck), lib=USER_LIB_PATH)
-        
-        if(is_installed(pck)){
-            print(paste0("Successfuly installed package: ", pck))
-        } else{
-            print(paste0("Failed to install package: ", pck))
-        }
     }
 }
 
