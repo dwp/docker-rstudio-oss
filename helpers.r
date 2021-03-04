04bd623c868a47c17b634dca4b395351c3f09d5c
draagc-iw <55280269+draagc-iw@users.noreply.github.com>
2021-03-04 23:57:33 +0000

    DW-5779 Configurable spark version (#29)
    * Ammend local package installation script to check for newer version of packages
    
    * Add helpers to Rprofile.site
    
    * Remove unnecessary local spark
    
    * Format helpers code
    
    * Add SPARK_VERSION to USER_PERSISTED_VARS

---
+++ helpers.r
@@ -0,0 +1,9 @@
+get_spark <- function(){
+    httr::set_config(httr::config(ssl_verifypeer = 0L, ssl_verifyhost = 0L))
+    conf <- sparklyr::spark_config(file = paste0("/home/", Sys.getenv("USER"),
+                                     "/.spark_config.yml"))
+    conf <- sparklyr::livy_config(conf = conf, proxy_user = Sys.getenv("USER"),
+                                executor_cores = 2, custom_headers = list(Authorization = Sys.getenv("JWT_TOKEN")))
+    sparklyr::spark_connect(method = "livy", version = Sys.getenv("SPARK_VERSION", unset="2.4.4"), config = conf)
+}
+
