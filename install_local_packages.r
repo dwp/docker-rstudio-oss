487f065a02a3770d1002dfcd491d07e2df8dd681
Dragos Costache <dragos.costache@infinityworks.com>
2021-04-08 12:26:27 +0100

    Roxygenize local packages before install

--- install_local_packages.r
+++ install_local_packages.r
@@ -1,44 +1,46 @@
-PACKAGES_PATH=Sys.getenv("PACKAGES_PATH", unset="/mnt/packages/r")
-USER_LIB_PATH=Sys.getenv("USER_LIB_PATH", unset=paste0("/home/", Sys.getenv("USER"), "/.rpckg"))
+PACKAGES_PATH <- Sys.getenv("PACKAGES_PATH", unset = "/mnt/packages/r")
+USER_LIB_PATH <- Sys.getenv("USER_LIB_PATH", unset = paste0("/home/", Sys.getenv("USER"), "/.rpckg"))
 
-packages <- list.files(path=PACKAGES_PATH)
-library(devtools)
-library(stringr)
+packages <- list.files(path = PACKAGES_PATH)
 library(desc)
 
 dir.create(USER_LIB_PATH, showWarnings = FALSE)
+tmp_work_path <- paste0("/tmp/pckg-install/")
+dir.create(tmp_work_path, showWarnings = FALSE)
 
-should_install <- function(package){
-    installed_packages <- installed.packages()
-    package_name <- str_replace(package, "(.*?)\\..*$", replacement="\\1")
+should_install <- function(package) {
+  installed_packages <- installed.packages()
+  package_name <- stringr::str_replace(package, "(.*?)\\..*$", replacement = "\\1")
 
-    if(!(package_name %in% installed_packages)){
-        print(paste0("Package ", package_name, " not currently installed. Installing..."))
-        return(TRUE)
-    }
+  if (!(package_name %in% installed_packages)) {
+    print(paste0("Package ", package_name, " not currently installed. Installing..."))
+    return(TRUE)
+  }
 
-    tmp_extract_path <- paste0("/tmp/pckg-install/")
-    dir.create(tmp_extract_path, showWarnings = FALSE)
-    untar(package, files = paste0(package_name, "/DESCRIPTION"), exdir=tmp_extract_path)
+  untar(paste0(PACKAGES_PATH, "/", package), files = paste0(package_name, "/DESCRIPTION"), exdir = tmp_work_path)
 
-    desc <- description$new(file=paste0(tmp_extract_path, package_name))
-    new_package_version <- toString(desc$get_version())
-    installed_package_version <- toString(installed_packages[package_name,  "Version"])
+  desc <- description$new(file = paste0(tmp_work_path, package_name))
+  new_package_version <- toString(desc$get_version())
+  installed_package_version <- toString(installed_packages[package_name, "Version"])
 
-    print(paste0(package_name, " - Installed package version: ", installed_package_version, ". New package version: ", new_package_version))
+  print(paste0(package_name, " - Installed package version: ", installed_package_version, ". New package version: ", new_package_version))
 
-    if(compareVersion(installed_package_version, new_package_version) == -1){
-        print(paste0("Package ", package_name, " is newer. Installing..."))
-        return(TRUE)
-    } else {
-        print(paste0("Package ", package_name, " is same version or older. Not installing."))
-        return(FALSE)
-    }
+  if (compareVersion(installed_package_version, new_package_version) == -1) {
+    print(paste0("Package ", package_name, " is newer. Installing..."))
+    return(TRUE)
+  } else {
+    print(paste0("Package ", package_name, " is same version or older. Not installing."))
+    return(FALSE)
+  }
 }
 
-for(pck in packages){
-    if(should_install(pck)) {
-        install_local(paste0(PACKAGES_PATH, "/", pck), lib=USER_LIB_PATH)
-    }
+for (pck_file in packages) {
+  if (should_install(pck_file)) {
+    pckg_path <- paste0(PACKAGES_PATH, "/", pck_file)
+    untar(pckg_path, exdir = tmp_work_path)
+    extracted_name <- stringr::str_replace(pck_file, "(.*?)\\..*$", replacement = "\\1")
+    devtools::document(paste0(tmp_work_path, extracted_name))
+    install.packages(paste0(tmp_work_path, extracted_name), repos = NULL, lib = USER_LIB_PATH)
+  }
 }
 
