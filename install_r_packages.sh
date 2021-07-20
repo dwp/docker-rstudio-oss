dbd7ffb30b67edfdb164904fda30ce5e2e80b2f4
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2021-07-20 08:59:10 +0100

    turns down install logs
    adds RODBCDBI pkg

--- install_r_packages.sh
+++ install_r_packages.sh
@@ -11,4 +11,4 @@ for i in ${R_PKGS[@]}; do
 done;
 
 PCK="${PCK%?}"; # Remove last comma
-R -e "install.packages(c(${PCK}), repos='https://cran.rstudio.com/', dependencies=TRUE, Ncpus=parallel::detectCores(), quiet=FALSE)"
+R -e "install.packages(c(${PCK}), repos='https://cran.rstudio.com/', dependencies=TRUE, Ncpus=parallel::detectCores(), quiet=TRUE)"
