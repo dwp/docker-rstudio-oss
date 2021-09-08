cc87463700b8bf50655d1d1052fa5acfaa2bd6a5
Sumit Bharadia <36968381+03ce007@users.noreply.github.com>
2021-09-08 10:57:00 +0100

    install packages using devtools which are ignored

--- install_r_packages.sh
+++ install_r_packages.sh
@@ -15,3 +15,8 @@ R -e "install.packages(c(${PCK}), repos='https://cran.rstudio.com/', dependencie
 
 # pin sparklyr version to 1.6.2
 R -e "require(devtools); install_version('sparklyr', version = '1.6.2', repos='http://cran.rstudio.com/', dependencies=T, Ncpus=parallel::detectCores(), quiet=TRUE)"
+
+# install packages using devtools which get ignored in `intall.packages` step
+R -e "require(devtools); install_version('plotly', version = '4.9.4.1', repos='http://cran.rstudio.com/', dependencies=T, Ncpus=parallel::detectCores(), quiet=FALSE)"
+R -e "require(devtools); install_version('factoextra', version = '1.0.7', repos='http://cran.rstudio.com/', dependencies=T, Ncpus=parallel::detectCores(), quiet=FALSE)"
+
