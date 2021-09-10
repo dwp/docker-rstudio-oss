8c764f886c983fda95d4ae162350ed1092e02546
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2021-09-10 09:04:47 +0100

    adds a check and re-installs using devtools those user requested pkgs which are ignored by standard install

--- install_r_packages.sh
+++ install_r_packages.sh
@@ -11,12 +11,26 @@ for i in ${R_PKGS[@]}; do
 done;
 
 PCK="${PCK%?}"; # Remove last comma
-R -e "install.packages(c(${PCK}), repos='https://cran.rstudio.com/', dependencies=TRUE, Ncpus=parallel::detectCores(), quiet=FALSE)"
 
-# pin sparklyr version to 1.6.2
+# issue a standard single command to install all *user requested* pkgs
+R -e "install.packages(c(${PCK}), repos='https://cran.rstudio.com/', dependencies=TRUE, Ncpus=parallel::detectCores(), quiet=TRUE)"
+
+# check if all the *user requested* pkgs are installed successfully or not
+# convert string pkg var defined earlier into an array
+declare -a PKGS=($(echo "${PCK}" | sed 's/\x27//g'| tr "," "\n"))
+# store output of in-built function `find.package()` when a pacakge is not found, into a var to compare later
+PKG_FAILED_STATUS="character(0)"
+# loop through all *user requested* pkgs
+for p in "${PKGS[@]}";
+do
+    # store only line 2 from the output
+    PKG_STATUS=$(R -q -e "find.package(\"${p}\", quiet=TRUE, verbose=TRUE)" | sed -n 2p)
+    if [ "${PKG_STATUS}" == "${PKG_FAILED_STATUS}" ]; then
+        # install using `devtools` when a package found that was missed using standard install function
+        R -e "require(devtools); install_version('"${p}"', repos='http://cran.rstudio.com/', dependencies=T, Ncpus=parallel::detectCores(), quiet=TRUE)"
+    fi
+done
+
+# always leave `sparklyr` package as last step to stop it getting updated accidently as dependency for some other pkg
+# and pin sparklyr version to 1.6.2
 R -e "require(devtools); install_version('sparklyr', version = '1.6.2', repos='http://cran.rstudio.com/', dependencies=T, Ncpus=parallel::detectCores(), quiet=TRUE)"
-
-# install packages using devtools which get ignored in `intall.packages` step
-R -e "require(devtools); install_version('plotly', version = '4.9.4.1', repos='http://cran.rstudio.com/', dependencies=T, Ncpus=parallel::detectCores(), quiet=FALSE)"
-R -e "require(devtools); install_version('factoextra', version = '1.0.7', repos='http://cran.rstudio.com/', dependencies=T, Ncpus=parallel::detectCores(), quiet=FALSE)"
-
